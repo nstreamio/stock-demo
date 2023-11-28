60c147ff8d7c6245a37db333e54fab30df4be88f
Jeremy Custenborder <jcustenborder@gmail.com>
2023-11-28 16:27:15 -0600

    Removed unecessary lanes for now. All data in status.

--- server/src/main/java/io/nstream/demos/stocks/agents/StockAgent.java
+++ server/src/main/java/io/nstream/demos/stocks/agents/StockAgent.java
@@ -22,80 +22,80 @@ import java.util.concurrent.TimeUnit;
 
 public class StockAgent extends AbstractAgent {
   private static final Logger log = LoggerFactory.getLogger(StockAgent.class);
-  @SwimLane("profile")
-  final ValueLane<Value> profile = this.valueLane();
+//  @SwimLane("profile")
+//  final ValueLane<Value> profile = this.valueLane();
   @SwimLane("status")
   final ValueLane<Value> status = this.valueLane();
-  @SwimLane("history")
-  final MapLane<Long, Value> history = this.mapLane();
+//  @SwimLane("history")
+//  final MapLane<Long, Value> history = this.mapLane();
 
 
   static final long HISTORY_TIMER_DURATION = TimeUnit.MINUTES.toMillis(1);
 
-  void fireHistoryTimer() {
-    try {
-      requestTimeSeries();
-    } catch (Exception ex) {
-      log.error("Exception thrown", ex);
-    } finally {
-      this.historyTimer = this.setTimer(HISTORY_TIMER_DURATION, this::fireHistoryTimer);
-    }
-  }
-
-  TimerRef historyTimer = this.setTimer(HISTORY_TIMER_DURATION, this::fireHistoryTimer);
+//  void fireHistoryTimer() {
+//    try {
+//      requestTimeSeries();
+//    } catch (Exception ex) {
+//      log.error("Exception thrown", ex);
+//    } finally {
+//      this.historyTimer = this.setTimer(HISTORY_TIMER_DURATION, this::fireHistoryTimer);
+//    }
+//  }
+//
+//  TimerRef historyTimer = this.setTimer(HISTORY_TIMER_DURATION, this::fireHistoryTimer);
 
 
   DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-  @SwimLane("updateTimeSeries")
-  final CommandLane<Value> updateTimeSeries = this.<Value>commandLane()
-      .onCommand(input -> {
-        Value meta = input.getSlot("meta");
-        String timezone = meta.getSlot("exchange_timezone").stringValue("America/New_York");
-        ZoneId tz = ZoneId.of(timezone);
-        Value values = input.getSlot("values");
+//  @SwimLane("updateTimeSeries")
+//  final CommandLane<Value> updateTimeSeries = this.<Value>commandLane()
+//      .onCommand(input -> {
+//        Value meta = input.getSlot("meta");
+//        String timezone = meta.getSlot("exchange_timezone").stringValue("America/New_York");
+//        ZoneId tz = ZoneId.of(timezone);
+//        Value values = input.getSlot("values");
+//
+//        values.forEach(value -> {
+//          String dt = value.getSlot("datetime").stringValue();
+//          LocalDateTime localDateTime = LocalDateTime.parse(dt, dateTimeFormatter);
+//          Instant instant = localDateTime.atZone(tz).toInstant();
+//
+//          List<String> fields = List.of("open", "high", "low", "close", "volume");
+//          Record historyValue = Record.of();
+//
+//          for (String field : fields) {
+//            Double v = value.getSlot(field).cast(Form.forDouble(), 0D);
+//            historyValue = historyValue.updated(field, v);
+//          }
+//          this.history.put(
+//              instant.toEpochMilli(),
+//              historyValue
+//          );
+//        });
+//      });
 
-        values.forEach(value -> {
-          String dt = value.getSlot("datetime").stringValue();
-          LocalDateTime localDateTime = LocalDateTime.parse(dt, dateTimeFormatter);
-          Instant instant = localDateTime.atZone(tz).toInstant();
+//  void requestTimeSeries() {
+//    Value request = Record.of()
+//        .slot("nodeUri", nodeUri().toString())
+//        .slot("lane", "updateTimeSeries")
+//        .slot("interval", "1min")
+//        .slot("symbol", getProp("symbol"));
+//
+//    this.command(Uri.parse("/adapter/twelvedata"), Uri.parse("timeSeries"), request);
+//
+//  }
+//
+//  @SwimLane("updateProfile")
+//  final CommandLane<Value> updateProfile = this.<Value>commandLane()
+//      .onCommand(this.profile::set);
 
-          List<String> fields = List.of("open", "high", "low", "close", "volume");
-          Record historyValue = Record.of();
-
-          for (String field : fields) {
-            Double v = value.getSlot(field).cast(Form.forDouble(), 0D);
-            historyValue = historyValue.updated(field, v);
-          }
-          this.history.put(
-              instant.toEpochMilli(),
-              historyValue
-          );
-        });
-      });
-
-  void requestTimeSeries() {
-    Value request = Record.of()
-        .slot("nodeUri", nodeUri().toString())
-        .slot("lane", "updateTimeSeries")
-        .slot("interval", "1min")
-        .slot("symbol", getProp("symbol"));
-
-    this.command(Uri.parse("/adapter/twelvedata"), Uri.parse("timeSeries"), request);
-
-  }
-
-  @SwimLane("updateProfile")
-  final CommandLane<Value> updateProfile = this.<Value>commandLane()
-      .onCommand(this.profile::set);
-
-  void requestProfileData() {
-    Value request = Record.of()
-        .slot("nodeUri", nodeUri().toString())
-        .slot("lane", "updateProfile")
-        .slot("symbol", getProp("symbol"));
-
-    this.command(Uri.parse("/adapter/twelvedata"), Uri.parse("profile"), request);
-  }
+//  void requestProfileData() {
+//    Value request = Record.of()
+//        .slot("nodeUri", nodeUri().toString())
+//        .slot("lane", "updateProfile")
+//        .slot("symbol", getProp("symbol"));
+//
+//    this.command(Uri.parse("/adapter/twelvedata"), Uri.parse("profile"), request);
+//  }
 
 
   @Override
@@ -107,18 +107,37 @@ public class StockAgent extends AbstractAgent {
 
   static class ValueBuilder {
     private final Value input;
-    public Value output = Record.of();
+    public Value output;
 
-    ValueBuilder(Value input) {
+    ValueBuilder(Value current, Value input) {
       this.input = input;
+
+      if(!current.isDefined()) {
+        this.output = Record.of();
+      } else {
+        this.output = current;
+      }
     }
 
-    public <T> void setIfDefined(String inputKey, String outputKey, Form<T> form, T defaultValue) {
+    public <T> void set(String inputKey, String outputKey, Form<T> form, T defaultValue) {
       Value inputValue = this.input.getSlot(inputKey);
+      Value outputValue = this.output.getSlot(outputKey);
+
+      if(inputValue.equals(outputValue)&&outputValue.isDefined()) {
+        return;
+      }
+
+      if(!inputValue.isDefined() && outputValue.isDefined()) {
+        return;
+      }
 
       if (null != inputValue && inputValue.isDefined()) {
         T value = inputValue.cast(form, defaultValue);
         this.output = this.output.updated(outputKey, Value.fromObject(value));
+      } else if (null == inputValue) {
+        this.output = this.output.updated(outputKey, Value.absent());
+      } else if(!inputValue.isDefined()) {
+        this.output = this.output.updated(outputKey, inputValue);
       }
     }
   }
@@ -126,10 +145,16 @@ public class StockAgent extends AbstractAgent {
   @SwimLane("update")
   final CommandLane<Value> update = this.<Value>commandLane()
       .onCommand(input -> {
-        ValueBuilder valueBuilder = new ValueBuilder(input);
-        valueBuilder.setIfDefined("timestamp", "timestamp", Form.forLong(), null);
-        valueBuilder.setIfDefined("price", "price", Form.forDouble(), null);
-        valueBuilder.setIfDefined("day_volume", "volume", Form.forDouble(), null);
-        this.status.set(valueBuilder.output);
+        ValueBuilder valueBuilder = new ValueBuilder(this.status.get(), input);
+        valueBuilder.set("timestamp", "timestamp", Form.forLong(), null);
+        valueBuilder.set("price", "price", Form.forDouble(), null);
+        valueBuilder.set("day_volume", "volume", Form.forDouble(), null);
+        valueBuilder.set("bid", "bid", Form.forDouble(), null);
+        valueBuilder.set("ask", "ask", Form.forDouble(), null);
+
+
+
+
+        this.status.set(valueBuilder.output.updated("movement", Value.absent()));
       });
 }
