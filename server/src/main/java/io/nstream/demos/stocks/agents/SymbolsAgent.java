ea5879e18dbf71244fe54244df11ce68ebbd89c2
Jeremy Custenborder <jcustenborder@gmail.com>
2024-03-14 12:23:13 -0500

    Rollback to 17 jvm because build is not ready.

--- server/src/main/java/io/nstream/demos/stocks/agents/SymbolsAgent.java
+++ server/src/main/java/io/nstream/demos/stocks/agents/SymbolsAgent.java
@@ -6,8 +6,16 @@ import swim.api.SwimLane;
 import swim.api.agent.AbstractAgent;
 import swim.api.lane.CommandLane;
 import swim.api.lane.JoinValueLane;
+import swim.structure.Item;
 import swim.structure.Value;
 import swim.uri.Uri;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SymbolsAgent extends AbstractAgent {
   private static final Logger log = LoggerFactory.getLogger(SymbolsAgent.class);
@@ -24,4 +32,44 @@ public class SymbolsAgent extends AbstractAgent {
         .laneUri("status")
         .open();
   });
+
+
+  LocalDate lastPreviousClose = LocalDate.EPOCH;
+
+  void requestPreviousClose() {
+    // Yesterday calculation
+    LocalDate today = ZonedDateTime.now(ZoneId.of("America/New_York"))
+        .toLocalDate();
+    LocalDate yesterday = today.minusDays(1);
+
+    if(DayOfWeek.SUNDAY == today.getDayOfWeek() || DayOfWeek.SATURDAY == today.getDayOfWeek()) {
+      log.trace("requestPreviousClose() - skipping be cause today is {}.", today.getDayOfWeek());
+      return;
+    }
+
+    if(!yesterday.isAfter(lastPreviousClose)) {
+      return;
+    }
+
+    final int SPLIT_SIZE = 50;
+    final List<String> symbols = new ArrayList<>(50);
+
+    this.stocks.keySet().forEach(symbol -> {
+      symbols.add(symbol);
+      if(SPLIT_SIZE == symbols.size()) {
+
+
+        symbols.clear();
+      }
+
+    });
+
+
+
+
+  }
+
+
+
+
 }
